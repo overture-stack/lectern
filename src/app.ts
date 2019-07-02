0cbfc62862190bb1390ccf15edebe4b132337ab3
Dušan Andrić <Dusan.Andric@gmail.com>
2019-07-02 11:03:50 -0400

    default mongo connection

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 import * as dictionaryController from "./controllers/dictionaryController";
 
 // Create Express server
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/lectern", { useNewUrlParser: true} ).then(
+    () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
+  ).catch(err => {
+    console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
+    // process.exit();
+  });
 
 app.set("port", 3000);
 
