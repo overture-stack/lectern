0dcc73954eb0c7e1e2a41e9774f79c112f21dbb9
Dušan Andrić <Dusan.Andric@gmail.com>
2019-07-19 15:40:17 -0400

    Adds swagger docs

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import bodyParser from "body-parser";
 import { serverPort } from "./config/serverConfig";
 import * as dictionaryController from "./controllers/dictionaryController";
 import { errorHandler } from "./utils/errors";
+import * as swaggerUi from "swagger-ui-express";
+import * as sagger from "./config/swagger.json";
 
 // Create Express server with mongoConfig
 const app = express();
@@ -13,6 +15,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(sagger));
 
 app.get("/", (_, res) => res.send("Lectern"));
 app.get("/dictionaries", dictionaryController.listDictionaries);
