4f4b08201199c97bac8963d101f1f316cfee7903
Dušan Andrić <Dusan.Andric@gmail.com>
2019-08-14 13:52:04 -0400

    staging deploy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -113,6 +113,26 @@ spec:
                 }
             }
         }
+
+        stage('Deploy to Overture Staging') {
+            when {
+                  branch "master"
+            }
+            steps {
+                container('helm') {
+                    withCredentials([file(credentialsId:'4ed1e45c-b552-466b-8f86-729402993e3b', variable: 'KUBECONFIG')]) {
+                        sh 'env'
+                        sh 'helm init --client-only'
+                        sh "helm ls --kubeconfig $KUBECONFIG"
+                        sh "helm repo add overture https://overture-stack.github.io/charts-server/"
+                        sh """
+                            helm upgrade --kubeconfig $KUBECONFIG --install --namespace=overture-staging lectern-overture-staging \\
+                            overture/lectern --reuse-values 
+                           """
+                    }
+                }
+            }
+        }
         
     }
 }
