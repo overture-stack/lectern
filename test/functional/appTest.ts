720d35abe292852abdf947cf6d41a274f611c48a
Dušan Andrić <Dusan.Andric@gmail.com>
2019-09-23 14:45:33 -0400

    ✅ Tests for mongoConfig and appConfig
    extra unit tests

---
+++ test/functional/appTest.ts
@@ -0,0 +1,36 @@
+import App from "../../src/app";
+import { expect } from "chai";
+import { AppConfig, getAppConfig } from "../../src/config/appConfig";
+
+describe("Test injection of config into Express App", () => {
+    it("Should have correct port and api docs path set", () => {
+        const testConfig: AppConfig = {
+            serverPort(): string {
+                return "54321";
+            },
+            openApiPath(): string {
+                return "/test-path";
+            },
+            mongoHost(): string {
+                return "localhost";
+            },
+            mongoPort(): string {
+                return "27017";
+            },
+            mongoUser(): string {
+                return undefined;
+            },
+            mongoPassword(): string {
+                return undefined;
+            },
+            mongoDb(): string {
+                return "lectern";
+            }
+        };
+
+        const app = App(testConfig);
+        const swaggerRoute = (app._router.stack.filter((layer: any) => layer.name == "swaggerInitFn"))[0];
+        expect(String(swaggerRoute.regexp)).to.contain("/test-path");
+        expect(app.get("port")).to.be.equal("54321");
+    });
+});
