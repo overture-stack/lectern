556284e6da04afde5e38abb1f7e45dc38b7e4c4b
Ciarán Schütte <ciaranschutte@gmail.com>
2021-12-10 14:01:56 -0500

    Feat/#472 normalize new lines (#151)
    * normalize schemas
    
    * fix test fixtures
    
    * fix string replacement
    
    * refactor to count for script being an array

---
+++ test/functional/schemaTest.ts
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2020 The Ontario Institute for Cancer Research. All rights reserved
+ *
+ * This program and the accompanying materials are made available under the terms of
+ * the GNU Affero General Public License v3.0. You should have received a copy of the
+ * GNU Affero General Public License along with this program.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import App from '../../src/app';
+import { expect } from 'chai';
+import { AppConfig, getAppConfig } from '../../src/config/appConfig';
+import { DictionaryDocument } from '../../src/models/Dictionary';
+import { normalizeSchema } from '../../src/services/schemaService';
+
+const input1 = require('./fixtures/linebreak/input1.json') as DictionaryDocument;
+const input2 = require('./fixtures/linebreak/input2.json') as DictionaryDocument;
+const output = require('./fixtures/linebreak/output.json') as DictionaryDocument;
+
+describe('Verify different EOL are normalized', () => {
+  it('Should not alter already formatted files', () => {
+    const normalizedSchema = normalizeSchema(input1.schemas[0]);
+    expect(normalizedSchema).to.deep.eq(output.schemas[0]);
+  });
+  it('Should output consistent script property', () => {
+    const normalizedSchema = normalizeSchema(input2.schemas[0]);
+    expect(normalizedSchema).to.deep.eq(output.schemas[0]);
+  });
+});
