00a95a303fc18dd0ca3d64c047f8261aaf5ba681
Mauricio Martinez <mmartinez@ebi.ac.uk>
2023-06-27 22:11:59 +0100

    173 - Use of references inside codeList (#183)
    * 173 - Use of references inside codeList
    
    - Add support for references in codeList.
    - Add support to references within references.
    
    * Code review comments
    
    - Test case for nested reference being an array.
    - Test self-references.
    - Fix typos.

--- src/utils/references.ts
+++ src/utils/references.ts
@@ -42,15 +42,6 @@ export const replaceReferences = (dictionary: DictionaryDocument) => {
  * @return schema clone with references replaced
  */
 export const replaceSchemaReferences = (schema: any, references: any) => {
-	const isReferenceValue = (value: string) => {
-		const regex = new RegExp('^#(/[-_A-Za-z0-9]+)+$');
-		return regex.test(value);
-	};
-
-	const referenceToObjectPath = (value: string) => {
-		return value.split('/').slice(1).join('.');
-	};
-
 	const clone = cloneDeep(schema);
 
 	const referenceSections = ['restrictions', 'meta'];
@@ -59,23 +50,103 @@ export const replaceSchemaReferences = (schema: any, references: any) => {
 		referenceSections.forEach((section) => {
 			for (const key in field[section]) {
 				const value = field[section][key];
-
-				if (isReferenceValue(value)) {
-					const reference = referenceToObjectPath(value);
-
-					const replaceValue = get(references, reference, undefined);
-
-					// Ensure we found a value, otherwise throw error for invalid reference
-					if (!replaceValue) {
-						throw new InvalidReferenceError(
-							`Unknown reference found - Schema: ${clone.name} Field: ${field.name} - Path: ${section}.${key} - Reference: ${reference} `,
-						);
+				const discovered = new Map<string, any>();
+				const visited = new Set<string>();
+				let replacedValue: any;
+				try {
+					replacedValue = replaceAllReferences(value, references, discovered, visited);
+				} catch (e) {
+					if (e instanceof InvalidReferenceError) {
+						const errorMessage = e.message + `. Schema: ${clone.name} Field: ${field.name} - Path: ${section}.${key}`
+						throw new InvalidReferenceError(errorMessage);
+					} else {
+						throw e; // re-throw the error unchanged
 					}
-
-					field[section][key] = replaceValue;
 				}
+				field[section][key] = replacedValue;
 			}
 		});
 	});
 	return clone;
 };
+
+// Check if an object or any of its elements is a reference and try to resolve it, taking into account they can be embedded. 
+// In `discovered` references that have been resolved are kept to avoid reprocessing, and `visited` helps to avoid cycles.
+const replaceAllReferences = (obj: any, references: any, discovered: Map<string, any>, visited: Set<string>): any => {
+	if (isTerminalSymbol(obj)) {
+		// The object could be a terminal symbol already, like a boolean, or a number. In that case, further search is not required.
+		return obj;
+	}
+	if (isReferenceValue(obj)) {
+		// If the object represents a reference, then try to resolve it, either by a direct look up or recursively.
+		return resolveReference(obj, references, discovered, visited);
+	} else if (Array.isArray(obj)) {
+		// If the object is an array, each element could potentially be a reference that ends up being resolved to one or more elements.
+		return replaceReferencesInArray(obj, references, discovered, visited);
+	} else {
+		// `obj` is an object, so each property is replaced with the resolved references
+		return replaceReferencesInObject(obj, references, discovered, visited);
+	}
+}
+
+const referenceToObjectPath = (value: string) => {
+	return value
+		.split('/')
+		.slice(1)
+		.join('.');
+};
+
+const isReferenceValue = (value: string) => {
+	const regex = new RegExp('^#(/[-_A-Za-z0-9]+)+$');
+	return regex.test(value);
+};
+
+// Checks if the symbol is already something that can be replaced in a reference
+const isTerminalSymbol = (value: any): boolean => {
+	return typeof value !== 'object' && !(isReferenceValue(value))
+}
+
+const resolveReference = (reference: string, references: any, discovered: Map<string, any>, visited: Set<string>): any => {
+	if (discovered.has(reference)) {
+		return discovered.get(reference)
+	}
+	const refPath = referenceToObjectPath(reference);
+	const replacement = get(references, refPath, undefined);
+	if (!replacement) {
+		throw new InvalidReferenceError(`Unknown reference found - Reference: ${refPath}`);
+	}
+	if (isTerminalSymbol(replacement)) {
+		discovered.set(reference, replacement);
+		return replacement;
+	} else {
+		if (visited.has(reference)) {
+			throw new InvalidReferenceError(`Cyclical references found - Reference: ${refPath}`);
+		}
+		visited.add(reference);
+		const resolvedReplacement = replaceAllReferences(replacement, references, discovered, visited);
+		discovered.set(reference, resolvedReplacement);
+		return resolvedReplacement;
+	}
+}
+
+const replaceReferencesInArray = (arr: any[], references: any, discovered: Map<string, any>, visited: Set<string>): any[] => {
+	const replacedArray: any[] = [];
+	arr.forEach(element => {
+		const replacedValue = replaceAllReferences(element, references, discovered, visited);
+		if (Array.isArray(replacedValue)) {
+			replacedArray.push(...replacedValue)
+		} else {
+			replacedArray.push(replacedValue)
+		}
+	});
+	return replacedArray;
+}
+
+const replaceReferencesInObject = (obj: any, references: any, discovered: Map<string, any>, visited: Set<string>): any[] => {
+	Object.entries(obj).forEach(([key, value]) => {
+		const replacedValue = replaceAllReferences(value, references, discovered, visited);
+		obj[key] = replacedValue;
+	});
+	return obj;
+}
+
