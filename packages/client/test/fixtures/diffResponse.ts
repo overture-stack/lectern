b656e5364e51c999e8085f7df8f81fe703671265
Jon Eubank <joneubank@gmail.com>
2024-06-10 21:45:27 -0400

    Organize tests and fixtures with clean names Also updated messages in tests that have changed during code cleanup

---
+++ packages/client/test/fixtures/diffResponse.ts
@@ -0,0 +1,317 @@
+import { DictionaryDiffArray } from 'dictionary';
+
+const diffResponse = [
+	[
+		'donor.submitter_donor_id',
+		{
+			left: {
+				description: 'Unique identifier of the donor, assigned by the data provider.',
+				name: 'submitter_donor_id',
+				restrictions: {
+					required: true,
+					regex: '[A-Za-z0-9\\-\\._]{1,64}',
+				},
+				valueType: 'string',
+			},
+			right: {
+				description: 'Unique identifier of the donor, assigned by the data provider.',
+				name: 'submitter_donor_id',
+				restrictions: {
+					required: true,
+					regex: '[A-Za-z0-9\\-\\._]{3,64}',
+				},
+				valueType: 'string',
+			},
+			diff: {
+				restrictions: {
+					regex: {
+						type: 'updated',
+						data: '[A-Za-z0-9\\-\\._]{3,64}',
+					},
+				},
+			},
+		},
+	],
+	[
+		'donor.vital_status',
+		{
+			left: {
+				description: 'Donors last known state of living or deceased.',
+				name: 'vital_status',
+				restrictions: {
+					codeList: ['Alive', 'Deceased', 'Not reported', 'Unknown'],
+					required: true,
+				},
+				valueType: 'string',
+			},
+			right: {
+				description: 'Donors last known state of living or deceased.',
+				name: 'vital_status',
+				restrictions: {
+					regex: '[A-Z]{3,100}',
+					required: true,
+				},
+				valueType: 'string',
+			},
+			diff: {
+				restrictions: {
+					codeList: {
+						type: 'deleted',
+						data: ['Alive', 'Deceased', 'Not reported', 'Unknown'],
+					},
+					regex: {
+						type: 'created',
+						data: '[A-Z]{3,100}',
+					},
+				},
+			},
+		},
+	],
+	[
+		'donor.cause_of_death',
+		{
+			left: {
+				description: "Description of the cause of a donor's death.",
+				name: 'cause_of_death',
+				restrictions: {
+					codeList: ['Died of cancer', 'Died of other reasons', 'Not reported', 'Unknown'],
+				},
+				valueType: 'string',
+			},
+			right: {
+				description: "Description of the cause of a donor's death.",
+				name: 'cause_of_death',
+				restrictions: {
+					codeList: ['Died of other reasons', 'Not reported', 'N/A'],
+				},
+				valueType: 'string',
+			},
+			diff: {
+				restrictions: {
+					codeList: {
+						type: 'updated',
+						data: {
+							added: ['N/A'],
+							deleted: ['Died of cancer', 'Unknown'],
+						},
+					},
+				},
+			},
+		},
+	],
+	[
+		'donor.survival_time',
+		{
+			left: {
+				description: 'Interval of how long the donor has survived since primary diagnosis, in days.',
+				meta: {
+					units: 'days',
+				},
+				name: 'survival_time',
+				valueType: 'integer',
+			},
+			right: {
+				description: 'Interval of how long the donor has survived since primary diagnosis, in days.',
+				meta: {
+					units: 'days',
+				},
+				name: 'survival_time',
+				valueType: 'integer',
+				restrictions: {
+					range: {
+						min: 0,
+						max: 200000,
+					},
+				},
+			},
+			diff: {
+				restrictions: {
+					type: 'created',
+					data: {
+						range: {
+							min: 0,
+							max: 200000,
+						},
+					},
+				},
+			},
+		},
+	],
+	[
+		'primary_diagnosis.cancer_type_code',
+		{
+			left: {
+				name: 'cancer_type_code',
+				valueType: 'string',
+				description:
+					'The code to represent the cancer type using the WHO ICD-10 code (https://icd.who.int/browse10/2016/en#/) classification.',
+				restrictions: {
+					required: true,
+					regex: '[A-Z]{1}[0-9]{2}.[0-9]{0,3}[A-Z]{0,1}$',
+				},
+			},
+			right: {
+				name: 'cancer_type_code',
+				valueType: 'string',
+				description:
+					'The code to represent the cancer type using the WHO ICD-10 code (https://icd.who.int/browse10/2016/en#/) classification.',
+				restrictions: {
+					required: true,
+					regex: '[A-Z]{1}[0-9]{2}.[0-9]{0,3}[A-Z]{2,3}$',
+				},
+			},
+			diff: {
+				restrictions: {
+					regex: {
+						type: 'updated',
+						data: '[A-Z]{1}[0-9]{2}.[0-9]{0,3}[A-Z]{2,3}$',
+					},
+				},
+			},
+		},
+	],
+	[
+		'primary_diagnosis.menopause_status',
+		{
+			left: {
+				name: 'menopause_status',
+				description: 'Indicate the menopause status of the patient at the time of primary diagnosis.',
+				valueType: 'string',
+				restrictions: {
+					codeList: ['Perimenopausal', 'Postmenopausal', 'Premenopausal', 'Unknown'],
+				},
+			},
+			diff: {
+				type: 'deleted',
+				data: {
+					name: 'menopause_status',
+					description: 'Indicate the menopause status of the patient at the time of primary diagnosis.',
+					valueType: 'string',
+					restrictions: {
+						codeList: ['Perimenopausal', 'Postmenopausal', 'Premenopausal', 'Unknown'],
+					},
+				},
+			},
+		},
+	],
+	[
+		'primary_diagnosis.presenting_symptoms',
+		{
+			left: {
+				name: 'presenting_symptoms',
+				description: 'Indicate presenting symptoms at time of primary diagnosis.',
+				valueType: 'string',
+				restrictions: {
+					codeList: ['Abdominal Pain', 'Anemia', 'Diabetes', 'Diarrhea', 'Nausea', 'None'],
+				},
+			},
+			right: {
+				name: 'presenting_symptoms',
+				description: 'Indicate presenting symptoms at time of primary diagnosis.',
+				valueType: 'string',
+				isArray: true,
+				restrictions: {
+					codeList: ['Abdominal Pain', 'Anemia', 'Diabetes', 'Diarrhea', 'Nausea', 'None'],
+				},
+			},
+			diff: {
+				isArray: {
+					type: 'created',
+					data: true,
+				},
+			},
+		},
+	],
+	[
+		'sample_registration.program_id',
+		{
+			left: {
+				name: 'program_id',
+				valueType: 'string',
+				description: 'Unique identifier of the ARGO program.',
+				meta: {
+					validationDependency: true,
+					primaryId: true,
+					examples: 'PACA-AU,BR-CA',
+					displayName: 'Program ID',
+				},
+				restrictions: {
+					required: true,
+				},
+			},
+			right: {
+				name: 'program_id',
+				valueType: 'integer',
+				description: 'Unique identifier of the ARGO program.',
+				meta: {
+					validationDependency: true,
+					primaryId: true,
+					examples: 'PACA-AU,BR-CA',
+					displayName: 'Program ID',
+				},
+				restrictions: {
+					required: true,
+				},
+			},
+			diff: {
+				valueType: {
+					type: 'updated',
+					data: 'integer',
+				},
+			},
+		},
+	],
+	[
+		'specimen.percent_stromal_cells',
+		{
+			left: {
+				name: 'percent_stromal_cells',
+				description:
+					'Indicate a value, in decimals, that represents the percentage of reactive cells that are present in a malignant tumour specimen but are not malignant such as fibroblasts, vascular structures, etc.',
+				valueType: 'number',
+				meta: {
+					dependsOn: 'sample_registration.tumour_normal_designation',
+					notes: '',
+					displayName: 'Percent Stromal Cells',
+				},
+				restrictions: {
+					range: {
+						min: 0,
+						max: 0.1,
+					},
+				},
+			},
+			right: {
+				name: 'percent_stromal_cells',
+				description:
+					'Indicate a value, in decimals, that represents the percentage of reactive cells that are present in a malignant tumour specimen but are not malignant such as fibroblasts, vascular structures, etc.',
+				valueType: 'number',
+				meta: {
+					dependsOn: 'sample_registration.tumour_normal_designation',
+					notes: '',
+					displayName: 'Percent Stromal Cells',
+				},
+				restrictions: {
+					range: {
+						max: 1,
+					},
+				},
+			},
+			diff: {
+				restrictions: {
+					range: {
+						min: {
+							type: 'deleted',
+							data: 0,
+						},
+						max: {
+							type: 'updated',
+							data: 1,
+						},
+					},
+				},
+			},
+		},
+	],
+] satisfies DictionaryDiffArray;
+export default diffResponse;
---
+++ packages/client/test/processing.spec.ts
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2024 The Ontario Institute for Cancer Research. All rights reserved
+ *
+ * This program and the accompanying materials are made available under the terms of
+ * the GNU Affero General Public License v3.0. You should have received a copy of the
+ * GNU Affero General Public License along with this program.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import chai from 'chai';
+import { functions as schemaService } from '../src';
+import { loggerFor } from '../src/logger';
+import { SchemaValidationErrorTypes } from '../src/validation';
+import dictionary from './fixtures/registrationSchema';
+const L = loggerFor(__filename);
+
+chai.should();
+
+const VALUE_NOT_ALLOWED = 'The value is not permissible for this field.';
+const PROGRAM_ID_REQ = 'program_id is a required field.';
+
+describe('processing', () => {
+	it('should populate records based on default value ', () => {
+		const result = schemaService.processRecords(dictionary, 'registration', [
+			{
+				program_id: 'PEME-CA',
+				submitter_donor_id: 'OD1234',
+				gender: '',
+				submitter_specimen_id: '87813',
+				specimen_type: 'Skin',
+				tumour_normal_designation: 'Normal',
+				submitter_sample_id: 'MAS123',
+				sample_type: 'ctDNA',
+			},
+			{
+				program_id: 'PEME-CA',
+				submitter_donor_id: 'OD1234',
+				gender: '',
+				submitter_specimen_id: '87812',
+				specimen_type: 'Skin',
+				tumour_normal_designation: 'Normal',
+				submitter_sample_id: 'MAS1234',
+				sample_type: 'ctDNA',
+			},
+		]);
+		chai.expect(result.processedRecords[0].gender).to.eq('Other');
+		chai.expect(result.processedRecords[1].gender).to.eq('Other');
+	});
+
+	it('should NOT populate missing columns based on default value ', () => {
+		const result = schemaService.processRecords(dictionary, 'registration', [
+			{
+				program_id: 'PEME-CA',
+				submitter_donor_id: 'OD1234',
+				gendr: '',
+				submitter_specimen_id: '87813',
+				specimen_type: 'Skin',
+				tumour_normal_designation: 'Normal',
+				submitter_sample_id: 'MAS123',
+				sample_type: 'ctDNA',
+			},
+			{
+				program_id: 'PEME-CA',
+				submitter_donor_id: 'OD1234',
+				gender: '',
+				submitter_specimen_id: '87812',
+				specimen_type: 'Skin',
+				tumour_normal_designation: 'Normal',
+				submitter_sample_id: 'MAS1234',
+				sample_type: 'ctDNA',
+			},
+		]);
+		chai.expect(result.validationErrors).to.deep.include({
+			errorType: SchemaValidationErrorTypes.MISSING_REQUIRED_FIELD,
+			fieldName: 'gender',
+			index: 0,
+			info: {},
+			message: 'gender is a required field.',
+		});
+	});
+});
