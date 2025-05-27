910be1cf9df6955a9dca01fa160c659fb3020eb3
Jon Eubank <joneubank@gmail.com>
2025-05-27 15:30:30 -0400

    Move lectern-ui package to /ui

--- packages/ui/stories/viewer-table/DictionaryHeader.stories.ts
+++ packages/ui/stories/viewer-table/DictionaryHeader.stories.ts
@@ -4,15 +4,6 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { pick } from 'lodash';
 import biosampleDictionary from '../fixtures/minimalBiosampleModel';
 
-const sampleField: SchemaField = {
-	name: 'hobbies',
-	valueType: 'string',
-	isArray: true,
-	delimiter: '|',
-	description: "List of user's favourite passtimes",
-	restrictions: {},
-};
-
 const meta = {
 	component: DictionaryHeader,
 	title: 'Viewer - Table/Dictionary Header',
