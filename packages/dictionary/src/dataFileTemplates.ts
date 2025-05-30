b1acee93e4889de65df134639186f0183483ef55
Azher2Ali <121898125+Azher2Ali@users.noreply.github.com>
2025-05-30 12:32:20 -0400

    #218: add endpoint to download TSV template files for all data entities (#252)
    * feat: add endpoint to download TSV template files for all data entities
    
    * Adding imports to package.json
    
    * Fixed the type errors
    
    * Improve type inference, error handling, accumulator typing and added swagger.json
    
    * Changes related to error response and route
    
    * Updated the pnpm-lock.yaml file to fix the built error due to mismatch
    
    * Reverting the pnpm-lock.yaml to the main and adding the recent import changes
    
    * Refactor: Addressing to the feedback and moving the template generation logic into lectern-dictonary
    
    * Removing quote property from the options
    
    * Refactor: Redirecting the routes for the imports and eliminating the usage of json2csv lib
    
    * fix: refactor: update Schema import path to './metaSchema/index'
    
    * fix(dictionaryController): correct JSZip file creation using template fileName and content
    
    * Template endpoint to have tsv/csv selection, matching file extensions
    
    * Fix comment typo
    
    * Use positive ternary logic where possible
    
    ---------
    
    Co-authored-by: Jon Eubank <joneubank@gmail.com>

---
+++ packages/dictionary/src/dataFileTemplates.ts
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2025 The Ontario Institute for Cancer Research. All rights reserved
+ *
+ * This program and the accompanying materials are made available under the terms of
+ * the GNU Affero General Public License v3.0. You should have received a copy of the
+ * GNU Affero General Public License along with this program.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import { z } from 'zod';
+import { Schema } from './metaSchema/index';
+
+export const separatedValueFileTypeSchema = z.enum(['tsv', 'csv']);
+export type SeparatedValueFileType = z.infer<typeof separatedValueFileTypeSchema>;
+
+export type DataFileTemplateConfig = { delimiter: string; extension: string };
+
+const SeparatedValueFileConfigs = {
+	csv: { delimiter: ',', extension: 'csv' },
+	tsv: { delimiter: '\t', extension: 'tsv' },
+} as const satisfies Record<SeparatedValueFileType, DataFileTemplateConfig>;
+
+export type CreateDataFileTemplateOptions = { fileType: SeparatedValueFileType } | DataFileTemplateConfig;
+export type DataFileTemplate = { fileName: string; content: string };
+
+/**
+ * Create the contents of a file that can be used to store data that will be validated with the provided Schema.
+ * The file will be a separated values file with a header row. The headers correspond to the fields in the schema.
+ * An empty row with placeholder delimiters for each field is also included. Also returned is the lectern standard
+ * filename for the file, which will use the schema name.
+ *
+ * By default this file will be a tab separated value file (`.tsv`). If an options object is provided, you can
+ * either specify the `fileType` as `tsv` or `csv`. Selecting `csv` will provide a comma separated value file
+ * (`.csv`) instead.
+ *
+ * If you do not specify a file type, the options also you to directly specify the file extension and delimiter value
+ * that is used.
+ *
+ * @param schema
+ * @param options - `{ fileType: 'csv' | 'tsv' }` OR `{ delimiter: string; extension: string; } `
+ * - default if not provided will use 'tsv'
+ * @returns
+ */
+export function createDataFileTemplate(schema: Schema, options?: CreateDataFileTemplateOptions): DataFileTemplate {
+	const config = options
+		? 'fileType' in options
+			? SeparatedValueFileConfigs[options.fileType]
+			: options
+		: SeparatedValueFileConfigs.tsv;
+
+	// Build header row from field names
+	const header = schema.fields.map((field) => field.name);
+
+	// Build a single empty data row (just placeholders)
+	const dataRow = schema.fields.map(() => '');
+
+	// Join header and row into TSV string
+	const lines = [header.join(config.delimiter), dataRow.join(config.delimiter)];
+
+	const content = lines.join('\n') + '\n';
+
+	return {
+		fileName: `${schema.name}.${config.extension}`,
+		content,
+	};
+}
