d0f4631efefd0d1b90ad1780fea77cbb6f048f74
Saqib Ashraf <ashrafsaqib40@gmail.com>
2025-06-10 14:43:35 -0400

    change console.log to alerts

--- packages/ui/stories/viewer-table/interaction-panel/ExpandAllButton.stories.tsx
+++ packages/ui/stories/viewer-table/interaction-panel/ExpandAllButton.stories.tsx
@@ -14,5 +14,5 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-	args: { setIsCollapsed: (isCollapsed: boolean) => console.log('all collapsable components are expanded') },
+	args: { setIsCollapsed: (isCollapsed: boolean) => alert('all collapsable components are expanded') },
 };
