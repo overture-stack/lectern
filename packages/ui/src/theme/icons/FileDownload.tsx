a7e293f5a69b13eedf5fb149dff2302c5246ad4b
Saqib Ashraf <ashrafsaqib40@gmail.com>
2025-06-16 11:08:10 -0400

    fix the icons

--- packages/ui/src/theme/icons/FileDownload.tsx
+++ packages/ui/src/theme/icons/FileDownload.tsx
@@ -22,10 +22,14 @@
 /** @jsxImportSource @emotion/react */
 
 import IconProps from './IconProps';
+import { css } from '@emotion/react';
 
-const FileDownload = ({ fill, height, width }: IconProps) => {
+const FileDownload = ({ fill, height, width, style }: IconProps) => {
 	return (
 		<svg
+			css={css`
+				${style}
+			`}
 			xmlns="http://www.w3.org/2000/svg"
 			width={width || '24'}
 			height={height || '24'}
