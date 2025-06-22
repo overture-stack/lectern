830cf3361ba541255267293a20ded60ab090bfd6
Saqib Ashraf <ashrafsaqib40@gmail.com>
2025-06-21 20:46:39 -0400

    capitalize the first letter since that is required in the figma

--- packages/ui/src/viewer-table/DataTable/SchemaTableInitialization/Columns/DataType.tsx
+++ packages/ui/src/viewer-table/DataTable/SchemaTableInitialization/Columns/DataType.tsx
@@ -5,5 +5,5 @@ import Pill from '../../../../common/Pill';
 
 export const renderDataTypeColumn = (type: CellContext<SchemaField, string>) => {
 	const { valueType, isArray } = type.row.original;
-	return <Pill>{isArray ? 'Array' : valueType}</Pill>;
+	return <Pill>{isArray ? 'Array' : valueType.charAt(0).toUpperCase() + valueType.slice(1)}</Pill>;
 };
