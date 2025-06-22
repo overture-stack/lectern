9792fa526d19fc5b75b3e5c8ffb3c7690c865849
Saqib Ashraf <ashrafsaqib40@gmail.com>
2025-06-21 22:25:40 -0400

    delimted requirement finish

--- packages/ui/src/viewer-table/DataTable/SchemaTableInitialization/Columns/DataType.tsx
+++ packages/ui/src/viewer-table/DataTable/SchemaTableInitialization/Columns/DataType.tsx
@@ -4,6 +4,10 @@ import { CellContext } from '@tanstack/react-table';
 import Pill from '../../../../common/Pill';
 
 export const renderDataTypeColumn = (type: CellContext<SchemaField, string>) => {
-	const { valueType, isArray } = type.row.original;
-	return <Pill>{isArray ? 'Array' : valueType.charAt(0).toUpperCase() + valueType.slice(1)}</Pill>;
+	const { valueType, isArray, delimiter } = type.row.original;
+	return (
+		<Pill>
+			{isArray ? `Array delimited by "${delimiter}"` : valueType.charAt(0).toUpperCase() + valueType.slice(1)}
+		</Pill>
+	);
 };
