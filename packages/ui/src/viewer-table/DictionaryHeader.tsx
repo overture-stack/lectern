726f548de89faf96c6b06125e753743bcf955d7f
Saqib Ashraf <121772695+SaqAsh@users.noreply.github.com>
2025-06-25 09:39:30 -0400

    #261: Implementation of Header Component  (#284)
    * finish up code cleanup and make sure that all styling is close to figma design
    
    * type theme appropriately instead of settling for any
    
    * fix styling and add comments for fonts
    
    * add some permuations of testing and reduce the threshold constants
    
    * final cleanup before pr, remove uneccesary styling
    
    * fix logic flaw
    
    * add minor spacing
    
    * remove tacky v
    
    * fix horizontal scrolling issue with css
    
    * address preceeding text spacing comment from pr
    
    * exporting component props
    
    * rename the constant to fit the naming standards better and add comments for clarity
    
    * address pr feedback

--- packages/ui/src/viewer-table/DictionaryHeader.tsx
+++ packages/ui/src/viewer-table/DictionaryHeader.tsx
@@ -20,12 +20,11 @@
  */
 
 /** @jsxImportSource @emotion/react */
-
 import { css } from '@emotion/react';
-import { ComponentType } from 'react';
-import colours from './styles/colours';
-
+import { useState } from 'react';
+import type { Theme } from '../theme';
 import { useThemeContext } from '../theme/ThemeContext';
+import colours from './styles/colours';
 
 export type DictionaryHeaderProps = {
 	name: string;
@@ -33,48 +32,118 @@ export type DictionaryHeaderProps = {
 	version?: string;
 };
 
-const DictionaryHeader: ComponentType<DictionaryHeaderProps> = ({ description, name }) => {
+const getChevronStyle = (isExpanded: boolean) => css`
+	margin-left: 4px;
+	${isExpanded && `transform: rotate(180deg);`}
+`;
+
+const linkStyle = (theme: Theme) => css`
+	${theme.typography?.subheading}
+	color: white;
+	cursor: pointer;
+	display: inline-flex;
+	align-items: center;
+
+	&:hover {
+		text-decoration: underline;
+	}
+`;
+
+// Was unable to find the appropriate font size for the version numbering in the current design system, that matches
+// Figma mockup so we are using something that is somewhat close with a hard coded font size
+
+const descriptionStyle = (theme: Theme) => css`
+	${theme.typography?.data}
+	font-size: 16px;
+	color: white;
+	margin: 0;
+	display: inline;
+`;
+
+const containerStyle = (theme: Theme) => css`
+	background-color: ${colours.accent1_1};
+	${theme.typography.heading}
+	display: flex;
+	margin-bottom: 1rem;
+	padding: 2.5rem;
+`;
+
+const rowLayoutStyle = css`
+	display: flex;
+	flex-direction: row;
+	width: 100%;
+`;
+
+const titleColumnStyle = css`
+	display: flex;
+	flex-direction: column;
+	flex: 1;
+	margin-right: 2rem;
+`;
+
+// Was unable to find the appropriate font size for the title in the current design system, that matches
+// Figma mockup so it is HARDCODED for now
+
+const titleStyle = css`
+	font-weight: 700;
+	font-size: 30px;
+	color: white;
+	line-height: 100%;
+	margin: 0;
+	margin-bottom: 0.5rem;
+`;
+
+// Was unable to find the appropriate font size for the version numbering in the current design system, that matches
+// Figma mockup so we are using something that is somewhat close
+
+const versionStyle = (theme: Theme) => css`
+	${theme.typography.data}
+	color: white;
+	font-size: 17px;
+`;
+
+const descriptionColumnStyle = css`
+	flex: 2;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+`;
+
+const DESCRIPTION_LENGTH_THRESHOLD = 140; // Chosen to display ~2-3 lines of text before truncation based on typical container width
+
+const DictionaryHeader = ({ name, description, version }: DictionaryHeaderProps) => {
 	const theme = useThemeContext();
+	const { ChevronDown } = theme.icons;
+	const [isExpanded, setIsExpanded] = useState(false);
+
+	// Determine if the description is long enough to need a toggle, based off of how many characters we want to show by default
+	// according to the figma styling
+	const needsToggle = description && description.length > DESCRIPTION_LENGTH_THRESHOLD;
+	// We want to show all the text if it is not long or if it is already expanded via state variable
+	const showFull = isExpanded || !needsToggle;
+	// Based off of showFull, we determine the text to show, either its the full description or a truncated version
+	const textToShow = showFull ? description : description.slice(0, DESCRIPTION_LENGTH_THRESHOLD) + '... ';
+
 	return (
-		<div
-			css={css`
-				background-color: ${colours.accent1_1};
-				${theme.typography.heading}
-				display: flex;
-				flex-direction: column;
-				width: 100%;
-				margin-bottom: 1rem;
-				padding: 2.5rem;
-				max-height: 10%;
-				align-items: flex-start;
-			`}
-		>
-			<div
-				css={css`
-					display: flex;
-					flex-direction: column;
-				`}
-			>
-				<h1
-					css={css`
-						font-weight: 700;
-						font-size: 40px;
-						color: white;
-						line-height: 100%;
-						margin: 0.5rem 0;
-					`}
-				>
-					{name}
-				</h1>
+		<div css={containerStyle(theme)}>
+			<div css={rowLayoutStyle}>
+				<div css={titleColumnStyle}>
+					<h1 css={titleStyle}>{name}</h1>
+					{version && <span css={versionStyle(theme)}>{version}</span>}
+				</div>
 				{description && (
-					<p
-						css={css`
-							color: white;
-							margin: 0;
-						`}
-					>
-						{description}
-					</p>
+					<div css={descriptionColumnStyle}>
+						<div>
+							<span css={descriptionStyle(theme)}>{textToShow}</span>
+							{needsToggle && (
+								<span css={linkStyle(theme)} onClick={() => setIsExpanded((prev) => !prev)}>
+									{' '}
+									{isExpanded ? 'Read less' : 'Show more'}
+									<ChevronDown style={getChevronStyle(isExpanded)} fill="white" width={10} height={10} />
+								</span>
+							)}
+						</div>
+					</div>
 				)}
 			</div>
 		</div>
