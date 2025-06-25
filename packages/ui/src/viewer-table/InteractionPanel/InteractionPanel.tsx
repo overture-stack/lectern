d0db70da287666ab3bfa5170751e0b801036cef5
Saqib Ashraf <ashrafsaqib40@gmail.com>
2025-06-25 10:24:08 -0400

    revert some cleanup since its messed up some stuff

--- packages/ui/src/viewer-table/InteractionPanel/InteractionPanel.tsx
+++ packages/ui/src/viewer-table/InteractionPanel/InteractionPanel.tsx
@@ -54,7 +54,11 @@ const panelStyles = (theme: Theme) => css`
 	border-bottom: 1px solid ${theme.colors.grey_4};
 	background-color: ${theme.colors.white};
 	min-height: 70px;
+	flex-wrap: nowrap;
+	overflow-x: auto;
+	overflow-y: visible;
 	box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+	position: relative;
 `;
 
 const sectionStyles = css`
