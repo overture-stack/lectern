bfbae7d681efd8bd3985961cb7e283f41c18554a
Saqib Ashraf <ashrafsaqib40@gmail.com>
2025-08-06 10:52:13 -0400

    some cleanup before demo

--- packages/ui/src/viewer-table/ConditionalLogicModal/ElseThenStatement.tsx
+++ packages/ui/src/viewer-table/ConditionalLogicModal/ElseThenStatement.tsx
@@ -74,7 +74,6 @@ export const ElseThenStatement = ({ restrictions, currentSchemaField, statementT
 			headerText={headerText}
 			simpleRestrictions={renderedSimpleRestrictions}
 			conditionalRestrictions={renderedConditionalRestrictions}
-			isContainer={conditionalRestrictions !== undefined && conditionalRestrictions.length > 0}
 		/>
 	);
 };
