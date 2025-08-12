e2c84288133b0696c4b7e5a9e09d2065373cd9c5
James Lopez <jamestlopez.code@gmail.com>
2025-08-12 10:47:30 -0400

    chore(import): remove unused import

--- apps/server/src/external/pcglAuthZClient.ts
+++ apps/server/src/external/pcglAuthZClient.ts
@@ -17,8 +17,6 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import { Request } from 'express';
-
 import { InternalServerError, ForbiddenError, NotFoundError } from '@overture-stack/lectern-dictionary';
 
 import { UserDataResponseErrorType } from '../common/types/auth';
