9c2d5437dd830dd13cfbb26989e7e877fbfce135
Ethan Luc <eluc@ML7010-ELUC.local>
2026-01-22 09:03:42 -0500

    style: optimize header visualy density

--- packages/ui/src/viewer-table/DataTable/TableHeader.tsx
+++ packages/ui/src/viewer-table/DataTable/TableHeader.tsx
@@ -31,7 +31,7 @@ const thStyle = (theme: Theme, index: number, width?: string) => css`
 	${theme.typography.tableHeader};
 	background: ${theme.colors.white};
 	text-align: ${index === 0 || index === 3 ? 'left' : 'center'};
-	padding: 12px;
+	padding: 6px 12px;
 	border-bottom: 1px solid ${theme.colors.border_medium};
 	border: 2px solid ${theme.colors.border_light};
 	${index === 0 &&
